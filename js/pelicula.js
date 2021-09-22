41bf61d363a73ba4effa01436a73c006a806b2cb
LuisCMr <81901473+MrLuisC15@users.noreply.github.com>
2021-09-22 10:58:17 +0200

    disparos

--- js/pelicula.js
+++ js/pelicula.js
@@ -9,9 +9,9 @@ class Pelicula{
         this.titulo = 'Tiroteo en Mississippi';
         this.pueblo = new Pueblo('TodoPolvo', 'muy polvoriento, en mitad del desierto de Arizona y a donde nadie querría llegar')
         this.narrador = new Narrador()
-        this.paco = new PersonajeBueno('Paco',3)
-        this.maria = new PersonajeBueno('Maria',2)
-        this.morgan = new PersonajeMalo('Morgan',4)
+        this.paco = new PersonajeBueno('Paco',3,1)
+        this.maria = new PersonajeBueno('Maria',2,1)
+        this.morgan = new PersonajeMalo('Morgan',4,1)
 
 
         this.iniciar();
@@ -31,10 +31,33 @@ class Pelicula{
 
         this.narrador.hablar('Maria ejecuta un disparo hacia Morgan')
         this.maria.arma.disparar()
-        this.narrador.hablar('La bala se va al aire sin darle a nadie')
-        this.narrador.hablar('Morgan dispara hacia Maria')
-        this.morgan.arma.disparar()
-        this.narrador.hablar('La bala le da al brazo derecho de Maria')
+        dado=(int)(Math.random()*10+1)
+        if(dado>=5) {
+            this.narrador.hablar('La bala impacta en el pecho de Morgan dejandolo abatido')
+            this.morgan.vivo=0;
+        }
+        else {
+            this.narrador.hablar('La bala se va al aire sin darle a nadie')
+        }
+
+        if(this.morgan.vivo==0) {
+            this.narrador.hablar('Todos serían felices y comerían perdices')
+        }
+        else {
+            this.narrador.hablar('Morgan dispara hacia Maria')
+            this.morgan.arma.disparar()
+            dado=(int)(Math.random()*10+1)
+            if(dado>=5) {
+                this.narrador.hablar('La bala impacta en el pecho de Maria dejandola abatida')
+                this.maria.vivo=0;
+            }
+            else {
+                this.narrador.hablar('La bala impacta en el pecho de Paco dejandolo abatido')
+                this.paco.vivo=0;
+                this.narrador.hablar('Maria huye de la zona a grandes velocidades')
+            }
+
+        }
     }
 
 }
@@ -53,9 +76,10 @@ class Narrador{
 }
 
 class Personaje{
-    constructor(nombre, balas) {
+    constructor(nombre, balas, vivo) {
         this.nombre=nombre
         this.arma=new Arma(balas)
+        this.vivo=vivo
     }
     
 }
@@ -78,8 +102,8 @@ class Arma{
     }
     disparar(){
         if(this.balas>0) {
-            document.write('<p>¡¡PUM!!'+this.balas+'</p>')
             this.balas--
+            document.write('<p>¡¡PUM!! - Balas restantes: '+this.balas+'</p>')
         }
         else {
             document.write('<p>Click...</p>')
