13829a45445719091232593d032f67259d6d484c
LuisCMr <81901473+MrLuisC15@users.noreply.github.com>
2021-09-22 09:28:53 +0200

    Titulo añadido

--- js/pelicula.js
+++ js/pelicula.js
@@ -4,7 +4,15 @@
 
 class Pelicula{
     constructor(){
-        console.log('Se ha creado una película');
+        //console.log('Se ha creado una película');
+        //This es el objeto que ejecuta el codigo
+        this.titulo = 'Tiroteo en Mississippi'
+
+        iniciar()
+
+    }
+    iniciar(){
+        document.write(this.titulo)
     }
 
 }
