d755b91ff66598b3b4c38d11546c88817fe66ada
LuisCMr <81901473+MrLuisC15@users.noreply.github.com>
2021-09-22 09:36:52 +0200

    añadida descripcion

--- js/pelicula.js
+++ js/pelicula.js
@@ -6,17 +6,32 @@ class Pelicula{
     constructor(){
         //console.log('Se ha creado una película');
         //This es el objeto que ejecuta el codigo
-        this.titulo = 'Tiroteo en Mississippi'
+        this.titulo = 'Tiroteo en Mississippi';
+        this.pueblo = new Pueblo('TodoPolvo', 'muy polvoriento, en mitad del desierto de Arizona y a donde nadie querría llegar')
+
+
 
         this.iniciar();
 
     }
 
     iniciar(){
-        document.write('<h1>' + this.titulo + '</h1>')
+        document.write('<h1>' + this.titulo + '</h1>');
+        document.write('<p>'+this.pueblo.nombre+' era un pueblo '+this.pueblo.descripcion)
     }
 
 }
+
+class Pueblo{
+    constructor(nombre, descripcion) {
+        this.nombre=nombre;
+        this.descripcion=descripcion;
+    }
+}
+
+
+
+
 new Pelicula()
 
 
