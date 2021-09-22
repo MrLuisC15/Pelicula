8dfeeb6e14ee53158f0d7d930261179185a497b4
LuisCMr <81901473+MrLuisC15@users.noreply.github.com>
2021-09-22 11:05:06 +0200

    dado

--- js/pelicula.js
+++ js/pelicula.js
@@ -34,7 +34,7 @@ class Pelicula{
 
         this.narrador.hablar('Maria ejecuta un disparo hacia Morgan')
         this.maria.arma.disparar()
-        dado=this.random(1,10)
+        let dado=this.random(1,10)
         if(dado>=5) {
             this.narrador.hablar('La bala impacta en el pecho de Morgan dejandolo abatido')
             this.morgan.vivo=0;
